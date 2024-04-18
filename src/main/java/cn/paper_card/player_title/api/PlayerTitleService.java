342217b64d9a7a8e6b4f5de0dd69115bbdb16128
Paper99 <1398872296@qq.com>
2024-04-18 16:37:15 +0800

    1.2 addOrUpdate

--- src/main/java/cn/paper_card/player_title/api/PlayerTitleService.java
+++ src/main/java/cn/paper_card/player_title/api/PlayerTitleService.java
@@ -11,11 +11,23 @@ public interface PlayerTitleService {
 
     boolean removeTitle(@NotNull String key) throws Exception;
 
+    /**
+     * 添加或更新称号
+     *
+     * @param key     称号的key
+     * @param content 称号的内容（格式代码）
+     * @return 添加返回true，更新返回false
+     * @throws Exception 操作失败抛出异常
+     */
+    boolean addOrUpdate(@NotNull String key, @NotNull String content) throws Exception;
+
     @Nullable TitleInfo queryByKey(@NotNull String key) throws Exception;
 
     @NotNull TitleInfo addPlayerTitle(@NotNull PlayerTitleInfo info) throws Exception;
 
     void removePlayerTitle(@NotNull String key, @NotNull UUID playerId) throws Exception;
+
+    boolean addOrUpdatePlayerTitle(@NotNull PlayerTitleInfo info) throws Exception;
 
     @Nullable PlayerTitleInfoInUse queryOnePlayerTitleInUse(@NotNull UUID uuid) throws Exception;
 
